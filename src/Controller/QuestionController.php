36d10f68375be4b8147c22a8bae8e487c6e4e1fa
Milos Obrenovic <mobrenovic040@gmail.com>
2021-10-31 15:42:36 +0100

    Commit

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController{
+
+
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage() {
+
+        return $this->render('question/homepage.html.twig');
+    }
+        /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     */
+    public function show($slug) {
+
+        $answers = [
+            "Nothin', just chillin', killin'..",
+            "Learning Symfony, wbu?",
+            "Checking SofaScore every 3 minutes.",
+
+        ];
+
+        return $this->render("question/show.html.twig", [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
+
+    }
+}
